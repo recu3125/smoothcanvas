6fc967be0178375c2dabd37818a14b4bf5a41c30
redstonecube <superhoy0509@gmail.com>
2022-08-22 16:17:46 +0900

    more smooth

--- canvas.js
+++ canvas.js
@@ -1,55 +1,71 @@
-const smoothnum = 100
+const smoothnum = 50
 const canvasresol = 3
 
 var canvas = document.getElementById('canvas')
 console.log(canvas)
 var ctx = canvas.getContext('2d');
-ctx.canvas.width = window.innerWidth*canvasresol;
-ctx.canvas.height = window.innerHeight*canvasresol;
+ctx.canvas.width = window.innerWidth * canvasresol;
+ctx.canvas.height = window.innerHeight * canvasresol;
 
 var mouse = { x: 0, y: 0 }
 var last_mouse = { x: 0, y: 0 }
 var mouselist = []
-var mousedown=0
+var mousedown = 0
 var beforexy = { x: 0, y: 0 }
 var mouseupdate = 1
 
-canvas.addEventListener('mousemove', function (e) {
-  if (mouseupdate==0) return;
-  mouse.x = typeof e.offsetX !== 'undefined' ? e.offsetX : e.layerX;
-  mouse.y = typeof e.offsetY !== 'undefined' ? e.offsetY : e.layerY;
-  mouse.x*=canvasresol
-  mouse.y*=canvasresol
-}, false); //마우스 위치 갱신
+document.onmousemove = handleMouseMove;
+function handleMouseMove(event) {
+  var eventDoc, doc, body;
+
+  event = event || window.event;
+
+  if (event.pageX == null && event.clientX != null) {
+    eventDoc = (event.target && event.target.ownerDocument) || document;
+    doc = eventDoc.documentElement;
+    body = eventDoc.body;
+
+    event.pageX = event.clientX +
+      (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+      (doc && doc.clientLeft || body && body.clientLeft || 0);
+    event.pageY = event.clientY +
+      (doc && doc.scrollTop || body && body.scrollTop || 0) -
+      (doc && doc.clientTop || body && body.clientTop || 0);
+  }
+  mouse.x = event.pageX
+  mouse.y = event.pageY
+  mouse.x *= canvasresol
+  mouse.y *= canvasresol
+  mouselist.shift()
+  mouselist.push({ x: mouse.x, y: mouse.y })
+}
 
 canvas.addEventListener('mousedown', linestart)
 canvas.addEventListener('mouseup', lineend)
 
-setInterval(draw,0)
+setInterval(draw, 20)
 
-function linestart()
-{
-  beforexy.x=mouse.x
-  beforexy.y=mouse.y
-  mousedown=1
+function linestart() {
+  beforexy.x = mouse.x
+  beforexy.y = mouse.y
+  mousedown = 1
   mouselist = []
   for (i = 0; i < smoothnum; i++) { //100까지 시작점으로 채움
     mouselist.push({ x: mouse.x, y: mouse.y })
   }
 }
 
-function lineend()
-{
-  mouseupdate=0
-  for(var i=0;i<=smoothnum;i++){
+function lineend() {
+  mouseupdate = 0
+  for (var i = 0; i <= smoothnum; i++) {
     draw()
   }
-  mouseupdate=1
-  mousedown=0
+  mouseupdate = 1
+  mousedown = 0
 }
 
 function draw() {
-  if(mousedown==0){
+  if (mousedown == 0) {
     return;
   }
   mouselist.shift()
@@ -62,17 +78,16 @@ function draw() {
 
   var xsum = 0
   var ysum = 0
-  for (var i=0;i<smoothnum;i++)
-  {
-    xsum+=mouselist[i].x
-    ysum+=mouselist[i].y
+  for (var i = 0; i < smoothnum; i++) {
+    xsum += mouselist[i].x
+    ysum += mouselist[i].y
   }
   ctx.beginPath()
-  afterx = xsum/smoothnum
-  aftery = ysum/smoothnum
-  ctx.moveTo(beforexy.x,beforexy.y)
-  ctx.lineTo(afterx,aftery)
-  beforexy.x=afterx
-  beforexy.y=aftery
+  afterx = xsum / smoothnum
+  aftery = ysum / smoothnum
+  ctx.moveTo(beforexy.x, beforexy.y)
+  ctx.lineTo(afterx, aftery)
+  beforexy.x = afterx
+  beforexy.y = aftery
   ctx.stroke()
 }
